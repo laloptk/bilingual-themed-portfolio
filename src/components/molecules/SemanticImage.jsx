b5dc4a361e4b3f095a6d9a3ea5bbd618a70933bb
Eduardo Sanchez <laloptk@users.noreply.github.com>
2025-06-01 00:36:34 -0600

    Add lazy loading to the images

--- src/components/molecules/SemanticImage.jsx
+++ src/components/molecules/SemanticImage.jsx
@@ -1,7 +1,7 @@
 const SemanticImage = ({ src = '', alt = '', caption }) => {
   return (
     <figure>
-      <img src={src} alt={alt} />
+      <img src={src} alt={alt} loading="lazy"/>
       <figcaption>{caption ? caption : alt}</figcaption>
     </figure>
   );
