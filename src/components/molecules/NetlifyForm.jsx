e32523e5195726d1fae5628e3666fe05b9efabda
Eduardo Sanchez <laloptk@users.noreply.github.com>
2025-06-04 09:53:25 -0600

    feat: update job entries in data.json with unique IDs and enhanced descriptions; add Netlify form component; adjust global styles for consistency

---
+++ src/components/molecules/NetlifyForm.jsx
@@ -0,0 +1,20 @@
+const NetlifyForm = () => {
+    return (
+        <form name="contact" netlify>
+            <p>
+                <label>Name <input type="text" name="name" /></label>
+            </p>
+            <p>
+                <label>Email <input type="email" name="email" /></label>
+            </p>
+            <p>
+                <label>Message<textarea></textarea></label>
+            </p>
+            <p>
+                <button type="submit">Send</button>
+            </p>
+        </form>
+    )
+}
+
+export default NetlifyForm
