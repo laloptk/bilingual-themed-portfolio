3650f42c5faaa87405736b802b93ca7471df5d4f
Eduardo Sanchez <laloptk@users.noreply.github.com>
2026-02-10 13:33:22 -0600

    Add Link element instead of anchor tags

--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -1,16 +1,18 @@
 import SemanticImage from "../molecules/SemanticImage";
 import LinksList from "../molecules/LinksList";
 import headerData from "../../assets/header.json";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header className="portfolio-header">
             <div class="portfolio-header--container">
+                <Link to="/">
                 {
                     headerData?.logo?.src &&
                         <SemanticImage src={headerData.logo.src} alt={headerData?.logo?.alt ?? "Website logo"} classes="main-logo" />
                 }
-            
+                </Link>
                 <nav className="main-nav">
                     {
                         headerData?.menu &&
